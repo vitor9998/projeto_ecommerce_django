04922e2c9a5cf065c86de5d5347f939eebdf5d10
Vitor Oikawa <vitor9998@gmail.com>
2021-02-25 17:54:45 -0300

    Criando os models Produto e Variação

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class VariacaoInLine(admin.TabularInline): #cria uma tabela na parte de baixo na área "Modificar produto" em admin.
+    model = models.Variacao
+    extra = 1 #exibir um campo extra em branco a mais.
+
+class ProdutoAdmin(admin.ModelAdmin): #Dentro do pruduto quais ilines terão; filhos que você quer editar junto.
+    inlines = [
+        VariacaoInLine
+    ]
+
+admin.site.register(models.Produto, ProdutoAdmin) #para aparecer no admin
+admin.site.register(models.Variacao)
