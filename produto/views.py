1bf60c685946f38c4a8742d2a0b41f2ca6db2ade
Vitor Oikawa <vitor9998@gmail.com>
2021-03-08 11:27:01 -0300

    Implementando o carrinho - Parte2

--- produto/views.py
+++ produto/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404   
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from django.contrib import messages
 from . import models
+
+from pprint import pprint
 
 class ListaProdutos(ListView):
     model = models.Produto
@@ -19,6 +21,10 @@ class DetalheProduto(DetailView):
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
+        # TODO: Remover linhas abaixo
+        # if self.request.session.get('carrinho'):
+        #     del self.request.session['carrinho']
+        #     self.request.session.save()
 
         #return redirect(self.request.META['HTTP_REFERER'])
 #Quando o usuário clickar em adicionar ao carrinho, vai fazer o que precisa como
@@ -37,6 +43,30 @@ class AdicionarAoCarrinho(View):
             return redirect(http_referer)
 
         variacao = get_object_or_404(models.Variacao, id=variacao_id)
+        variacao_estoque = variacao.estoque
+        produto = variacao.produto
+
+        produto_id = produto.id
+        produto_nome = produto.nome
+        variacao_nome = variacao.nome or ''
+        preco_unitario = variacao.preco
+        preco_unitario_promocional = variacao.preco_promocional
+        quantidade = 1
+        slug = produto.slug
+        imagem = produto.imagem
+
+        if imagem:
+            imagem = imagem.name
+        else:
+            imagem = ''
+
+        if variacao.estoque < 1:
+            messages.error(
+                self.request,
+                'Estoque insuficiente'
+            )
+            return redirect(http_referer)
+
 
         if not self.request.session.get('carrinho'): #Estou tentando obter a chave carrinho da sessão
             self.request.session['carrinho'] = {}
@@ -45,23 +75,57 @@ class AdicionarAoCarrinho(View):
         carrinho = self.request.session['carrinho']
 
         if variacao_id in carrinho:
-            # TODO: Variação existe no carrinho
-            pass
+            quantidade_carrinho = carrinho[variacao_id]['quantidade']
+            quantidade_carrinho += 1
+
+            if variacao_estoque < quantidade_carrinho:
+                messages.warning(
+                    self.request,
+                    f'Estoque insuficiente para {quantidade_carrinho}x no'
+                    f'produto "{produto_nome}". Adicionamos {variacao_estoque}x'
+                    f'no seu carrinho.'
+                )
+                quantidade_carrinho = variacao_estoque
+
+            carrinho[variacao_id]['quantidade'] = quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo'] = preco_unitario * \
+                 quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo_promocional'] = preco_unitario_promocional * \
+                 quantidade_carrinho
         else:
-            # TODO:Variação não existe no carrinho
-            pass
+            carrinho[variacao_id] = {
+                'produto_id' : produto_id,
+                'produto_nome': produto_nome,
+                'variacao_nome': variacao_nome,
+                'variacao_id': variacao_id,
+                'preco_unitario': preco_unitario,
+                'preco_unitario_promocional': preco_unitario_promocional,
+                'preco_quantitativo': preco_unitario,
+                'preco_quantitativo_promocional': preco_unitario_promocional,
+                'quantidade' : 1,
+                'slug': slug,
+                'imagem' : imagem,
+            }
+        self.request.session.save()
+
+        messages.success(
+            self.request,
+            f'Produto {produto_nome} {variacao_nome} adicionado ao seu'
+            f' carrinho {carrinho[variacao_id]["quantidade"]}x.'
+        )
+
 #Cookies são arquivos que ficam do lado do cliente que salvam dados úteis
 #para aplicação, cookies são únicos para cada pessoa, sessões são tipo cookies mas a
 #diferença é que são salvas no lado do servidor (Resumidamente). Por padrão o django
 # não salva em um arquivo no servidor, ele vai salvar na base de dados.
-        return HttpResponse(f'{variacao.produto} {variacao.nome}')
+        return redirect(http_referer)
 
 class RemoverDoCarrinho(View):
     def get(self, *args, **kwargs):
         return HttpResponse('Remover carrinho')
 class Carrinho(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('Carrinho')
+        return render(self.request, 'produto/carrinho.html')
 class Finalizar(View):
     def get(self, *args, **kwargs):
         return HttpResponse('Finalizar')
