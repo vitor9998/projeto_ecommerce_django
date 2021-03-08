7db464792dadaf7ac02817397683379fa8cabb72
Vitor Oikawa <vitor9998@gmail.com>
2021-03-08 15:47:11 -0300

    Renderizando o carrinho - Parte2

--- utils/utils.py
+++ utils/utils.py
@@ -2,3 +2,7 @@
 #evitado repetição
 def formata_preco(val):
     return f'R$ {val:.2f}'.replace('.', ',')
+
+
+def cart_total_qtd(carrinho):
+    return sum([item['quantidade'] for item in carrinho.values()])
