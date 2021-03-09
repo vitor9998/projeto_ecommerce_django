c161885fd693a38fe29061ad5534ce43828c7a01
Vitor Oikawa <vitor9998@gmail.com>
2021-03-09 14:19:56 -0300

    Implementando cadastro de usuários - Parte3

--- perfil/forms.py
+++ perfil/forms.py
@@ -15,6 +15,12 @@ class UserForm(forms.ModelForm):
         label='Senha'
 
     )
+    password2 = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+        label='Confirmação senha'
+
+    )
     def __init__(self, usuario=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -22,8 +28,52 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'password', 'email')
+        fields = ('first_name', 'last_name', 'username', 'password',
+                    'password2', 'email')
 
     def clean(self, *args, **kwargs):
         data = self.data
         cleaned = self.cleaned_data
+        validation_error_msgs = {}
+
+        usuario_data = cleaned.get('username')
+        email_data = cleaned.get('email')
+        password_data = cleaned.get('password')
+        password2_data = cleaned.get('password2')
+
+
+        usuario_db = User.objects.filter(username=usuario_data).first()
+        email_db = User.objects.filter(email=email_data).first()
+
+        error_msg_user_exists = 'Usuário já existe'
+        error_msg_user_exists = 'E-mail já existe'
+        error_msg_password_match = 'As duas senhas não conferem.'
+        error_msg_password_short = 'Sua senha precisa de pelo menos 6 caracteres.'
+
+        # Usuários logados: atualizacão
+        if self.usuario:
+            if usuario_db:
+                if usuario_data != usuario_db.username:
+                    validation_error_msgs['username'] = error_msg_user_exists
+
+            if email_db:
+                if email_data != email_db.email:
+                    validation_error_msgs['email'] = error_msg_email_exists
+
+            if password_data:
+                if password_data != password2_data:
+                    validation_error_msgs['password'] = error_msg_password_match
+                    validation_error_msgs['password2'] = error_msg_password_match
+
+                if len(password_data) < 6:
+                    validation_error_msgs['password'] = error_msg_password_short
+
+        # Usuários não logados: cadastro
+        else:
+            validation_error_msgs['username'] = 'Bla bla bla'
+
+        if validation_error_msgs:
+            raise(forms.ValidationError(validation_error_msgs))
+
+
+
