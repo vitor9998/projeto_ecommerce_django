86823294f8dba988ddadaeb619f8646b35cdf15b
Vitor Oikawa <vitor9998@gmail.com>
2021-03-12 17:57:05 -0300

    Salvando o pedido - Parte1

--- pedido/views.py
+++ pedido/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 
 class Pagar(View):
+    template_name = 'pedido/pagar.html'
+
     def get(self, *args, **kwargs):
-        return HttpResponse('Pagar')
+
+        contexto = {
+
+        }
+
+        return render(self.request, self.template_name, contexto)
+
+
 class SalvarPedido(View):
     def get(self, *args, **kwargs):
         return HttpResponse('FecharPedido')
+
+
 class Detalhe(View):
     def get(self, *args, **kwargs):
         return HttpResponse('Detalhe')
