e7b08bd293657497e78892243052e2936777af90
Vitor Oikawa <vitor9998@gmail.com>
2021-03-15 10:57:51 -0300

    Salvando o pedido - Parte4

--- pedido/views.py
+++ pedido/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 from django.contrib import messages
 
 from produto.models import Variacao
+from .models import Pedido, ItemPedido
+
+from utils import utils
 
 class Pagar(View):
     template_name = 'pedido/pagar.html'
@@ -33,18 +36,65 @@ class Pagar(View):
         )
 
         for variacao in bd_variacoes:
-            vid = variacao.id
+            vid = str(variacao.id)
+
+
 
             estoque = variacao.estoque
             qtd_carrinho = carrinho[vid]['quantidade']
             preco_unt = carrinho[vid]['preco_unitario']
             preco_unt_promo = carrinho[vid]['preco_unitario_promocional']
 
+
+            error_msg_estoque = ''
+
             if estoque < qtd_carrinho:
                 carrinho[vid]['quantidade'] = estoque
                 carrinho[vid]['preco_quantitativo'] = estoque * preco_unt
                 carrinho[vid]['preco_quantitativo_promocional'] = estoque * \
                      preco_unt_promo
+
+
+                error_msg_estoque = 'Estoque insuficiente para alguns produtos do seu carrinho. '\
+                'Reduzimos a quantidade desses produtos. Por favor verifique '\
+                'quais produtos foram afetados a seguir.'
+
+            if error_msg_estoque:
+                messages.error(
+                    self.request,
+                    error_msg_estoque
+                )
+                self.request.session.save()
+                return redirect('produto:carrinho')
+
+        qtd_total_carrinho = utils.cart_total_qtd(carrinho)
+        valor_total_carrinho = utils.cart_totals(carrinho)
+
+        pedido = Pedido(
+            usuario=self.request.user,
+            total=valor_total_carrinho,
+            qtd_total=qtd_total_carrinho,
+            status='C',
+
+        )
+
+        pedido.save()
+
+        ItemPedido.objects.bulk_create(
+            [
+                ItemPedido(
+                    pedido=pedido,
+                    produto=v['produto_nome'],
+                    produto_id=v['produto_id'],
+                    variacao=v['variacao_nome'],
+                    variacao_id=v['variacao_id'],
+                    preco=v['preco_quantitativo'],
+                    preco_promocional=v['preco_quantitativo_promocional'],
+                    quantidade=v['quantidade'],
+                    imagem=v['imagem'],
+                ) for v in carrinho.values()
+            ]
+        )
 
         contexto = {
 
